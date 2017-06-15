105eb11aebf7a726dee5f48a89447b3be17e947e
Mathew Hall <mhmathewhall@gmail.com>
2017-06-15 10:18:43 +0100

    Video Player added

--- src/components/option1.js
+++ src/components/option1.js
@@ -17,11 +17,21 @@ class Option1 extends Component {
         <SectionHeader header="Showcase." />
         <div className="container">
           <Showcase
+            title="React Video Player"
+            picture1={require("../assets/videoplayer1.png")}
+            picture2={require("../assets/videoplayer2.PNG")}
+            text="A React based video player that uses the YouTube API."
+            logo={require("../assets/reactnative.png")}
+            link="https://mhall-video-player.herokuapp.com/"/>
+            <hr></hr>
+          <Showcase
+            title="React Stopwatch"
             picture1={require("../assets/reactstopwatch1.png")}
             picture2={require("../assets/reactstopwatch2.png")}
-            text="This is a React based stopwatch adapted from a React Native project."
+            text="A React based stopwatch adapted from a React Native project."
             logo={require("../assets/reactnative.png")}
             link="https://mhall-react-stopwatch.herokuapp.com/"/>
+            <hr></hr>
         </div>
       </div>
     );
--- src/components/showcase.js
+++ src/components/showcase.js
@@ -8,12 +8,15 @@ class Showcase extends Component {
   render(){
     return(
       <div>
+        <div className="row showcase showcase-title">
+          <h3>{this.props.title}</h3>
+        </div>
         <div className="row showcase">
           <div className="left-div col-sm-6">
-            <img src={this.props.picture1} />
+            <a href={this.props.link}><img src={this.props.picture1} /></a>
           </div>
           <div className="right-div col-sm-6">
-            <img src={this.props.picture2} />
+            <a href={this.props.link}><img src={this.props.picture2} /></a>
           </div>
         </div>
         <div className="row showcase">
@@ -23,7 +26,6 @@ class Showcase extends Component {
           <div className="right-div col-sm-6 second-row">
             <a href={this.props.link}><img src={this.props.logo} /></a>
           </div>
-          <hr/>
         </div>
       </div>
     );
