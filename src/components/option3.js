7d7552cb283e095df23f91ffeaf46785e810ca69
Mathew Hall <mhmathewhall@gmail.com>
2017-06-20 08:35:46 +0100

    Social items updated

--- src/components/option3.js
+++ src/components/option3.js
@@ -16,9 +16,9 @@ class Option3 extends Component {
       <div id="con3" className="content">
         <SectionHeader header="Contact." />
         <div className="content-container">
-          <Social text="LinkedIn" logo={require("../assets/linkedin.png")} link="https://www.linkedin.com/in/mhallcouk/" />
-          <Social text="Twitter" logo={require("../assets/twitter.svg")} link="https://twitter.com/mhallcouk" />
-          <Social text="GitHub" logo={require("../assets/GitHub.png")} link="https://github.com/mhallcouk" />
+          <Social text="LinkedIn" logo={require("../assets/linkedin.png")} link="https://www.linkedin.com/in/mhallxyz/" />
+          <Social text="Twitter" logo={require("../assets/twitter.svg")} link="https://twitter.com/mhallxyz" />
+          <Social text="GitHub" logo={require("../assets/GitHub.png")} link="https://github.com/mhallxyz" />
         </div>
       </div>
     );
